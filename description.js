2ab57021ff007692431ad0494f762f4fd98db819
Joey Ballentine <joeyjballentine@gmail.com>
2020-01-18 14:50:25 -0600

    Added description parsing for youtube videos with timestamps in description
    Also removed permissions because they break everything

--- description.js
+++ description.js
@@ -1,3 +1,43 @@
+const api_key = 'AIzaSyChX7b0VFxndHfnqsbMCXRFXzVmMTBlTcQ';
+
 window.onload = () => {
-    $('#description').children().text('C0rnH4ck5');
+    let video_id = window.location.search.split('v=')[1];
+    let ampersandPosition = video_id.indexOf('&');
+    if (ampersandPosition != -1) {
+        video_id = video_id.substring(0, ampersandPosition);
+    }
+    let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${video_id}&fields=items/snippet/title,items/snippet/description&key=${api_key}`;
+    console.log(url);
+
+    fetch(url)
+        .then((data) => {
+            return data.json();
+        })
+        .then((response) => {
+            let description = response.items[0].snippet.description;
+            parseDescription(description);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    // $('#description')
+    //     .children()
+    //     .text('C0rnH4ck5');
 };
+
+function parseDescription(description) {
+    let lines = description.split('\n');
+    let timestamps = [];
+    for (line of lines) {
+        //console.log(line);
+        const regex = /[1-9]?[0-9]:[0-9][0-9]/;
+        if (regex.test(line)) {
+            let text = line.replace(regex, '');
+            let stamp = line.match(regex)[0];
+            console.log({ text, stamp });
+            timestamps.push({ text, stamp });
+        }
+    }
+    return timestamps;
+}
