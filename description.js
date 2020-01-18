7c4dace045dcd481f8949fe1e481130e17f84321
Samir Kashyap <samir.kashyap17@gmail.com>
2020-01-18 14:13:52 -0600

    Added way to get current time of video

--- description.js
+++ description.js
@@ -1,3 +1,11 @@
+var timestamps = [];
+ytplayer = document.getElementsByClassName("ytp-time-current");
+
 window.onload = () => {
     $('#description').children().text('C0rnH4ck5');
-};
+};  
+
+onmousedown = () => {
+    timestamps.push(ytplayer[0].innerText);
+    console.log(timestamps)
+}
