164ba759c6b3dcee701e81de42123437d89a2545
ssparks <sdsparks98@gmail.com>
2020-01-18 18:15:47 -0600

    Refactored

--- description.js
+++ description.js
@@ -1,45 +1,16 @@
-var timestamps = [];
-ytplayercurrent = document.getElementsByClassName('ytp-time-current');
-ytplayertotal = document.getElementsByClassName('ytp-time-duration');
 const api_key = 'AIzaSyChX7b0VFxndHfnqsbMCXRFXzVmMTBlTcQ';
+let duration;
 
 window.onload = () => {
-    totalTime = calculateTime(ytplayertotal[0].innerText.toString().split(":"));
-    let video_id = window.location.search.split('v=')[1];
-    if (video_id.includes('&')) {
-        let ampersandPosition = video_id.indexOf('&');
-        if (ampersandPosition != -1) {
-            video_id = video_id.substring(0, ampersandPosition);
-        }
-    }
-    let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${video_id}&fields=items/snippet/title,items/snippet/description&key=${api_key}`;
-    console.log(url);
-
-    fetch(url)
-        .then((data) => {
-            return data.json();
-        })
-        .then((response) => {
-            let description = response.items[0].snippet.description;
-            let duration = $('.ytp-time-duration').text();
-            console.log(duration);
-            parseDescription(description);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-    onmousedown = () => {
-        timestamps.push(ytplayercurrent[0].innerText);
-        currentTime = calculateTime(ytplayercurrent[0].innerText.toString().split(":"));
-        timestamps.push(currentTime);
-        placement = currentTime/totalTime;
-        console.log(timestamps);
-        console.log(placement);
-        // let left = placement*100;
-        // console.log(left);
-        // addMarker(left);
-    };
+    clearMarkers();
+    doSomething();
+  window.addEventListener("yt-navigate-start", () => {
+  //chrome.webNavigation.onHistoryStateUpdated( () => {
+  duration = $('.ytp-time-duration').text();
+    clearMarkers();
+    doSomething();
+  });
+  //doSomething();
 };
 
 function addMarker(percentage) {
@@ -56,7 +27,6 @@ function parseDescription(description) {
         if (regex.test(line)) {
             let text = line.replace(regex, '');
             let stamp = line.match(regex)[0];
-            addMarker(timeToPercentage(stamp));
             console.log({ text, stamp });
             timestamps.push({ text, stamp });
         }
@@ -64,9 +34,47 @@ function parseDescription(description) {
     return timestamps;
 }
 
+function doSomething() {
+  let video_id = window.location.search.split('v=')[1];
+  if (video_id && video_id.includes('&')) {
+      let ampersandPosition = video_id.indexOf('&');
+      if (ampersandPosition != -1) {
+          video_id = video_id.substring(0, ampersandPosition);
+      }
+  }
+  let url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${video_id}&fields=items/snippet/title,items/snippet/description&key=${api_key}`;
+  console.log(url);
+
+  fetch(url)
+      .then((data) => {
+          return data.json();
+      })
+      .then((response) => {
+          let description = response.items[0].snippet.description;
+          let newStamps = parseDescription(description);
+          for(stamp of newStamps) {
+            let percentage = calculateTimePercentage(stamp.stamp,duration)
+            console.log(percentage);
+            addMarker(percentage);
+          }
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+}
+
+function clearMarkers() {
+  $("div").remove(".ytstmp-mrkr")
+};
+
+function calculateTimePercentage(currentTime, totalTime){
+    return (calculateTime(currentTime) / calculateTime(totalTime)) * 100.0;
+}
+
 function calculateTime(time){
+    time = time.split(":");
     let totalTime = 0;
-    let position = time.length - 1; 
+    let position = time.length - 1;
     let multiplier = 1;
     while(position >= 0){
         totalTime += time[position] * multiplier;
@@ -75,3 +83,5 @@ function calculateTime(time){
     }
     return totalTime;
 }
+
+//addMarker(calculateTimePercentage(stamp,duration));
