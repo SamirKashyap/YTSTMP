ab41bcf5fd361b9c110c931f00de89d9c93b6729
Joey Ballentine <joeyjballentine@gmail.com>
2020-01-18 21:25:53 -0600

    Added ads and skipping them

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,21 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.todo == "showIcon"){
-        chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
-            chrome.pageAction.show(tabs[0].id);
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+//     if(request.todo == "showIcon"){
+//         chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+//             chrome.pageAction.show(tabs[0].id);
+//         });
+//     }
+// });
+
+//Listen for when a Tab changes state
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo && changeInfo.status == "complete") {
+        //console.log("Tab updated: " + tab.url);
+
+        chrome.tabs.sendMessage(tabId, {
+            data: tab
+        }, function (response) {
+            console.log(response);
         });
+
     }
 });
